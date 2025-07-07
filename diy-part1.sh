caffc9687885eefd3ec60ee0dae866c0e94867bc
tgdh520 <59686937+tgdh520@users.noreply.github.com>
2025-07-08 01:21:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git 5G-Modem-Support https://github.com/Siriling/5G-Modem-Support.git' >>feeds.conf.default
+echo 'src-git 5G-Modem-Support https://github.com/Siriling/5G-Modem-Support' >>feeds.conf.default
