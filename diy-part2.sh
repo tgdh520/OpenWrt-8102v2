d4f57251009a312ef1684fa8342f8359b38e6c0d
tgdh520 <59686937+tgdh520@users.noreply.github.com>
2026-06-28 00:10:34 +0800

    Update WiFi settings in diy-part2.sh
    Uncomment and modify WiFi configuration settings in diy-part2.sh.

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,14 @@
 # See /LICENSE for more information.
 #
 
+#sed -i '317s/GMT0/CST-8/g' package/base-files/files/bin/config_generate
+#sed -i '318s|UTC|Asia/Shanghai|g' package/base-files/files/bin/config_generate
+#sed -i '166s|192.168.1.1|10.0.0.1|g' package/base-files/files/bin/config_generate
+#sed -i '167s|192.168|10.0|g' package/base-files/files/bin/config_generate
+#默认开启Wifi
+sed -i '115s|0 : 1|1 : 0|g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+#修改默认Wifi名称
+sed -i '112s|OpenWrt|ZBT8102|g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
