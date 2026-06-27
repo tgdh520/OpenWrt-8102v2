b64e87c8db0c9c35f0d9b9c594c796c2f9375cd2
tgdh520 <59686937+tgdh520@users.noreply.github.com>
2026-06-27 23:37:58 +0800

    Uncomment and modify feed source configurations

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,19 +12,18 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i "/helloworld/d" "feeds.conf.default"
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 #sed -i '317s/GMT0/CST-8/g' package/base-files/files/bin/config_generate
 #sed -i '318s|UTC|Asia/Shanghai|g' package/base-files/files/bin/config_generate
 #sed -i '166s|192.168.1.1|10.0.0.1|g' package/base-files/files/bin/config_generate
 #sed -i '167s|192.168|10.0|g' package/base-files/files/bin/config_generate
 #默认开启Wifi
-#sed -i '115s|0 : 1|1 : 0|g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i '115s|0 : 1|1 : 0|g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 #修改默认Wifi名称
-#sed -i '112s|OpenWrt|ZBT8102|g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i '112s|OpenWrt|ZBT8102|g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >> feeds.conf.default
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
