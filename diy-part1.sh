e4ed42b2daa0b66e2e81fe0dbe0adbd5fc30a642
tgdh520 <59686937+tgdh520@users.noreply.github.com>
2026-06-30 08:40:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,11 +15,11 @@
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
-sed -i "/passwall2/d" "feeds.conf.default"
-echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git" >> "feeds.conf.default"
+#sed -i "/passwall2/d" "feeds.conf.default"
+#echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git" >> "feeds.conf.default"
 
-sed -i "/OpenClash/d" "feeds.conf.default"
-echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> "feeds.conf.default"
+#sed -i "/OpenClash/d" "feeds.conf.default"
+#echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> "feeds.conf.default"
 # Add a feed source
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >> feeds.conf.default
